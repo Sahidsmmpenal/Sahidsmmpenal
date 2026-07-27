4919d3cf0b50af8872d46fd6abbdf5921f5971f5
sm8237715-art <sm8237715@gmail.com>
2026-07-27 20:35:04 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+export const CONFIG = {
+    API_URL: "https://your-smm-provider.com/api/v2", // Ekhane apnar provider-er API endpoint bosaben
+    API_KEY: "YOUR_SMM_PROVIDER_API_KEY"             // Ekhane apnar provider-er API Key bosaben
+};
