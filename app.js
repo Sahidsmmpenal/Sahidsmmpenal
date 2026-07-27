9e81821b223dcbcf1f5f19d466d87bdd2e65087f
sm8237715-art <sm8237715@gmail.com>
2026-07-27 16:19:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
-// SAHID SMM PANEL - App Script
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import {
   getAuth,
-  GoogleAuthProvider,
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   signInWithPopup
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 import {
-  getFirestore
+  getFirestore,
+  doc,
+  setDoc
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBELsMTiY8OqZE5zJqwUThrfUCSu13gG7U",
-  authDomain: "sahidsmmpenal.firebaseapp.com",
-  projectId: "sahidsmmpenal",
-  storageBucket: "sahidsmmpenal.firebasestorage.app",
-  messagingSenderId: "278872394021",
-  appId: "1:278872394021:web:0309da42a62c36195472cc",
-  measurementId: "G-B17NWVEBP3"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
